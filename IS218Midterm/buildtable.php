345fd08be65c5a612bedf55de0761574fd3df410
edelinearagon <44731323+edelinearagon@users.noreply.github.com>
2019-04-09 05:49:31 -0400

    Update buildtable.php
    fixed errors 
    error: public static function --> function

--- IS218Midterm/buildtable.php
+++ IS218Midterm/buildtable.php
@@ -5,53 +5,44 @@
  * Date: 2019-03-21
  * Time: 04:58
  */
-
-use csvtoarray;
-use printcsvarrray;
-use readcsv;
-
-class html
-{    
-    public static function buildTable(Array $wordstab)
+function buildTable($wordstab)
+{
+    $row = 1;
+    if (($wordstab) !== FALSE)
     {
-     $row = 1;
-        if (($wordstab) !== FALSE)
-        {
 
-            echo '<table border="1">';
-            //this one creates a table first, and keeps creating rows as there is data
-            while ($wordstab !== FALSE) {
-                $num = count($wordstab);
-                if ($row == 1) {
-                    echo '<thead><tr>';
+        echo '<table border="1">';
+        //this one creates a table first, and keeps creating rows as there is data
+        while ($wordstab !== FALSE) {
+            $num = count($wordstab);
+            if ($row == 1) {
+                echo '<thead><tr>';
+            }else{
+                echo '<tr>';
+            }
+            //puts data in the cells
+            for ($c=0; $c < $num; $c++) {
+                if(empty($wordstab[$c])) {
+                    $value = "&nbsp;";
                 }else{
-                    echo '<tr>';
+                    $value = $wordstab[$c];
                 }
-                //puts data in the cells
-                for ($c=0; $c < $num; $c++) {
-                    if(empty($wordstab[$c])) {
-                        $value = "&nbsp;";
-                    }else{
-                        $value = $wordstab[$c];
-                    }
-                    if ($row == 1) {
-                        echo '<th>'.$value.'</th>';
-                    }else{
-                        echo '<td>'.$value.'</td>';
-                    }
-                }
-
                 if ($row == 1) {
-                    echo '</tr></thead><tbody>';
+                    echo '<th>'.$value.'</th>';
                 }else{
-                    echo '</tr>';
+                    echo '<td>'.$value.'</td>';
                 }
-                $row++;
-                }
+            }
 
-            echo '</tbody></table>';
+            if ($row == 1) {
+                echo '</tr></thead><tbody>';
+            }else{
+                echo '</tr>';
+            }
+            $row++;
         }
 
+        echo '</tbody></table>';
     }
-    
-?>
+
+}
